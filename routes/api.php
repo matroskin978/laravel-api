854364856acb68a39fce8e80f3dc94a3ab12eff1
Админ <matroskin8@i.ua>
2024-04-27 12:34:08 +0300

    Lesson 3

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/v1/categories', [\App\Http\Controllers\Api\V1\CategoryController::class, 'index']);
+
+Route::prefix('v1')->group(function () {
+    Route::apiResource('categories', \App\Http\Controllers\Api\V1\CategoryController::class);
+});
+
+
