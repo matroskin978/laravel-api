7195d1f6a621c8b84d31e1d10d94ef7b1c5c8bbd
Админ <matroskin8@i.ua>
2024-04-30 12:07:11 +0300

    Lesson 9

--- routes/api.php
+++ routes/api.php
@@ -7,10 +7,15 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::prefix('v1')->middleware(['throttle:api'])->group(function () {
+    Route::post('register', [\App\Http\Controllers\Api\V1\AuthController::class, 'register']);
+    Route::post('login', [\App\Http\Controllers\Api\V1\AuthController::class, 'login']);
+});
 
 Route::prefix('v1')->middleware(['throttle:api', 'auth:sanctum'])->group(function () {
     Route::apiResource('categories', \App\Http\Controllers\Api\V1\CategoryController::class);
     Route::apiResource('posts', \App\Http\Controllers\Api\V1\PostController::class);
+    Route::get('logout', [\App\Http\Controllers\Api\V1\AuthController::class, 'logout']);
 });
 
 
